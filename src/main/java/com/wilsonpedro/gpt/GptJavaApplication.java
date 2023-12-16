4eec5f8dec42051f119f8ec6d41642a77fc9450e
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-12-16 10:22:14 -0300

    first commit

---
+++ src/main/java/com/wilsonpedro/gpt/GptJavaApplication.java
@@ -0,0 +1,13 @@
+package com.wilsonpedro.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilsonpedro/gpt/GptJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonpedro.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
